01820a22da8128b84638ae83ffdfc3585336b08e
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 17:47:48 +0400

    Change example status to out of maintenance

--- CS/WebSite/Default.aspx.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Web.UI.WebControls;
-
-public partial class _Default : System.Web.UI.Page {
-    public string getClientID(string colName, string controlName) {
-        TextBox t = (TextBox)grid.FindHeaderTemplateControl(grid.Columns[colName], controlName);
-        return t.ClientID;
-    }
-}
